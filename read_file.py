ecf6fc73c6ea79e412513b33d1a8cb631c8746a0
Henry Hsieh <lucky9456@msn.com>
2021-07-11 16:09:28 +0800

    new

--- read_file.py
+++ read_file.py
@@ -12,10 +12,14 @@ Created on Sun Jul 11 15:40:38 2021
 #f file
 
 data = []
+count = 0
 with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line.strip()) 
         #strip:去除空格及換行符號
+        count += 1
+        if count % 1000 == 0:
+            print(len(data))
         
 print(len(data))
 print(data[67])
