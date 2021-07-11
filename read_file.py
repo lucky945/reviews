bbf592106c3a05ccf956089a6025531f31e5c564
Henry Hsieh <lucky9456@msn.com>
2021-07-11 16:29:17 +0800

    add

--- read_file.py
+++ read_file.py
@@ -13,14 +13,22 @@ Created on Sun Jul 11 15:40:38 2021
 
 data = []
 count = 0
+length = 0
 with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line.strip()) 
         #strip:去除空格及換行符號
+        length = length + len(line)
         count += 1
         if count % 1000 == 0:
             print(len(data))
         
-print(len(data))
-print(data[67])
-    
+
+print('檔案讀取完畢，總共有：', len(data),'筆data')
+print('留言平均長度：', length / len(data))
+
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print(len(new))
